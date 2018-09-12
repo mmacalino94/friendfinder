0d54d146db7869dde4e7768aa9241bdf1d88e85c
White Lily <mmacalino03@gmail.com>
2018-09-11 21:52:17 -0400

    installed npms

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,5 +1,5 @@
 var path = require("path");
-var friends=require("../app/data/friends.js");
+var friends = require("../app/data/friends.js");
 
 module.exports = function(app) {
   app.get("/api/friends", function(req, res) {
