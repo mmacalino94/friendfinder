ca72112fd5ffc2f5b6754512747e784bd3ac410e
White Lily <mmacalino03@gmail.com>
2018-09-11 21:45:18 -0400

    fixed routes

---
+++ routing/apiRoutes.js
@@ -0,0 +1,43 @@
+var path = require("path");
+var friends=require("../app/data/friends.js");
+
+module.exports = function(app) {
+  app.get("/api/friends", function(req, res) {
+       res.json(friends);
+  })
+
+app.post("/api/friends", function(req, res) {
+  console.log(req.body)
+  var bestfriend={
+    name: "",
+    photo: "",
+    friendDifference: Infinity
+  }
+ 
+    var newFriend = req.body;
+    var newFriendResponses = newFriend.scores;
+    var totdiff;//
+
+
+for (var i = 0; i < friends.length; i++) {
+var currentfrnd=friends[i];
+totdiff=0;
+ for (var j = 0; j < currentfrnd.scores.length; j++) {
+  var currentFriendScore = currentfrnd.scores[j];
+  var currentUserScore = newFriendResponses[j];
+
+  totdiff += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
+  }
+
+  if (totdiff <= bestfriend.friendDifference) {
+    bestfriend .name = currentfrnd.name;
+    bestfriend .photo = currentUserScore.photo;
+    bestfriend .friendDifference = totdiff;
+  }
+}
+
+friends.push(newFriend);
+
+res.json(bestfriend );
+  });
+}
