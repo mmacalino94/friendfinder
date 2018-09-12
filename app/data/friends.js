dd8b4681aae3344d83281c4df161642728e2ea14
White Lily <mmacalino03@gmail.com>
2018-09-11 21:32:17 -0400

    entered gitignore and home.html

---
+++ app/data/friends.js
@@ -0,0 +1,16 @@
+var friends = [{
+    name: "Sora",
+    photo: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiBysj3p7TdAhUB3IMKHYtrDyQQjRx6BAgBEAU&url=https%3A%2F%2Fwww.zerochan.net%2F1284683&psig=AOvVaw0-Zh-SNcKRFqZFfd_M7awI&ust=1536802035481414",
+    scores: [
+        3,
+        4,
+        5,
+        5,
+        2,
+        2,
+        3,
+        1,
+        4,
+        1
+    ]
+}]
